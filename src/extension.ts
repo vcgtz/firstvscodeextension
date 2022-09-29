0c40b314b5648b490d2524ab7c73c0d5a5cacf9e
Vicente Gutiérrez <gtz.rvera@gmail.com>
2022-09-28 20:56:30 -0500

    Update inital message

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('firstvscodeextension.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from FirstVSCodeExtension!');
+		vscode.window.showInformationMessage('Hello World from my first VS Code Extension!');
 	});
 
 	context.subscriptions.push(disposable);
